5ef6ed68329c79a51bd0c45bce86366981182dd0
RupanwitaM <72640910+RupanwitaM@users.noreply.github.com>
2020-10-15 08:28:43 +0530

    Create to check prime no. to be expressed as sum of two prime no.s

---
+++ to check prime no. to be expressed as sum of two prime no.s
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+bool Primecheck(int n);
+
+int main() {
+	// check if a number can be expressed as sum of 2 prime numbers
+	// take input from user
+	int num,i;
+	bool flagnum = false;
+
+	cout << "Enter a positive  integer: ";
+	cin >> num;
+	
+	for(i = 2; i <= num/2; ++i) {
+        if (Primecheck(i)) {
+            if (Primecheck(num - i)) {
+                cout << num << " = " << i << " + " << num-i << endl;
+                flagnum = true;
+            }
+        }
+    }
+	
+	if (!flagnum)
+      cout << num << " can't be expressed as sum of two prime numbers.";
+
+    return 0;
+}
+	
+	// to check prime number
+bool Primecheck(int num)
+{
+    int i;
+    bool Prime = true;
+
+    // 0 and 1 are not prime numbers
+    if (num == 0 || num == 1) {
+        Prime = false;
+    }
+    else {
+        for(i = 2; i <= num/2; ++i) {
+            if(num % i == 0) {
+                Prime = false;
+                break;
+            }
+        }
+    }
+
+    return Prime;
+}
